87b77e0d8527aa08bc391fda73e094f19faa0780
paul2k724-web <paula@pauldevverse.com>
2026-02-27 15:09:15 +0530

    add cache control headers

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
